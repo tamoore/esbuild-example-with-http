f0848e009e27c85a427ace20246f5c8138ed6c39
Todd Moore <11851685+tamoore@users.noreply.github.com>
2021-01-17 10:36:33 +0000

    Add css and html templates

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
-	"github.com/evanw/esbuild/pkg/api"
-	"github.com/tamoore/esbuild-plugins/pkg/http"
 	"log"
 	"os"
+
+	"github.com/evanw/esbuild/pkg/api"
+	"github.com/tamoore/esbuild-plugins/pkg/http"
 )
 
 func main() {
 	result := api.Build(api.BuildOptions{
-		EntryPoints: []string{"src/app.jsx"},
+		Color:             0,
+		ErrorLimit:        0,
+		LogLevel:          0,
+		Sourcemap:         0,
+		SourcesContent:    0,
+		Target:            0,
+		Engines:           []api.Engine{},
+		MinifyWhitespace:  true,
+		MinifyIdentifiers: true,
+		MinifySyntax:      true,
+		Charset:           0,
+		TreeShaking:       0,
+		JSXFactory:        "",
+		JSXFragment:       "",
 		Define: map[string]string{
 			"process.env.NODE_ENV": "\"production\"",
 		},
-		Bundle:            true,
-		Outfile:           "out.js",
-		MinifyWhitespace:  true,
-		MinifySyntax:      true,
-		MinifyIdentifiers: true,
-		Plugins:           []api.Plugin{http.Plugin},
+		Pure:       []string{},
+		AvoidTDZ:   false,
+		KeepNames:  false,
+		GlobalName: "",
+		Bundle:     true,
+		Splitting:  false,
+		Outfile:    "",
+		Metafile:   "",
+		Outdir:     "build",
+		Outbase:    "",
+		Platform:   0,
+		Format:     0,
+		External:   []string{},
+		MainFields: []string{},
+		Loader: map[string]api.Loader{
+			".html": api.LoaderText,
+			".css":  api.LoaderText,
+		},
+		ResolveExtensions: []string{},
+		Tsconfig:          "",
+		OutExtensions:     map[string]string{},
+		PublicPath:        "",
+		Inject:            []string{},
+		Banner:            "",
+		Footer:            "",
+		EntryPoints:       []string{"src/app.jsx"},
+		Stdin:             &api.StdinOptions{},
 		Write:             true,
+		Incremental:       false,
+		Plugins:           []api.Plugin{http.Plugin},
 	})
 
 	if len(result.Errors) > 0 {
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,17 @@
 import { default as identity } from "https://dev.jspm.io/lodash@4.17.15/identity";
 import { default as React } from "https://dev.jspm.io/react@17.0.1";
 import { default as Server } from "https://dev.jspm.io/react-dom@17.0.1/server";
+import Template from "./example.html";
+import styles from "./example.css";
+
+// Output styles for a template string
+console.log(styles);
+
+// Output a possible template string
+console.log(Template);
 
 // Output lodash call
-console.log(identity("test"));
+console.log(identity("<lodash identity call>"));
 
 // Output render to string
 let Greet = () => <h1>Hello, world!</h1>;
